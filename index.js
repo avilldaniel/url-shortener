0a32b7dab503ea6e1bd316a7d5934f4d845d1288
Daniel Villegas <avilldaniel@Daniels-MacBook-Pro.local>
2022-06-19 21:49:58 -0700

    add taxi icon

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
-
-
 const API_URL = "https://url-shortener-node-express.herokuapp.com";
-const pasteForm = document.querySelector('#paste-form');
-
-// const pasteURL = form.querySelector('paste-url');
+const content = document.querySelector('.content');
+const pasteForm = document.querySelector('.fullURL');
+const verifyForm = document.querySelector('.verifyURL');
 
 pasteForm.addEventListener('submit', async e => {
+  // store text input
   e.preventDefault();
   let pasteURL = pasteForm.querySelector('#paste-url').value.trim();
-  console.log(pasteURL);
-  // (async () => {
-  //   const res = await axios.post(`${API_URL}/shorten/${pasteURL}`);
-  //   console.log(res);
-  // })
+  
+  // POST method to API endpoint
   const res = await axios.post(`${API_URL}/shorten/${pasteURL}`);
-  console.log(res);
+  const code = content.querySelector('.code');
+  code.innerText = res.data;
+  if(code.style.display === "none") code.style.display = "block";
 })
 
-// extract shortening-icon.png from backend
+verifyForm.addEventListener('submit', async e => {
+  // store text input
+  e.preventDefault();
+  let verifyURL = verifyForm.querySelector('input').value.trim();
+
+  // GET method to API endpoint
+  const res = await axios.get(`${API_URL}/${verifyURL}`);
+  const link = verifyForm.querySelector('a');
+  link.innerText = res.data;
+  link.setAttribute('href', res.data);
+  if(link.style.display === "none") link.style.display = "block";
+})
+
+// fetch shortening-icon.png from backend
 // const path = require("path");
 // const dir = path.join(__dirname, "public");
 // app.use(express.static(dir));
+// https://url-shortener-node-express.herokuapp.com/shortening-icon.png
